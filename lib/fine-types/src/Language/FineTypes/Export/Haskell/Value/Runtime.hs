64463f8db8ce1c7c0dbb4059493b621dfeab1890
paolino <paolo.veronelli@gmail.com>
2023-09-25 07:46:37 +0000

    Add ToTyp class and instances for Zero types and primitive structures

--- lib/fine-types/src/Language/FineTypes/Export/Haskell/Value/Runtime.hs
+++ lib/fine-types/src/Language/FineTypes/Export/Haskell/Value/Runtime.hs
@@ -1,21 +1,26 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use bimap" #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- | Export values of Haskell types to 'Value'.
 module Language.FineTypes.Export.Haskell.Value.Runtime
     ( ToValue (..)
     , V.Value (..)
+    , ToTyp (..)
     ) where
 
 import Prelude ((.))
 
-import qualified Language.FineTypes.Value as V
+import Data.Bifunctor (bimap)
+import Data.Proxy (Proxy (..))
+import Language.FineTypes.Typ (Typ)
 
 import qualified Data.ByteString
 import qualified Data.Map
 import qualified Data.Set
 import qualified Data.Text
+import qualified Language.FineTypes.Typ as Typ
+import qualified Language.FineTypes.Value as V
 import qualified Numeric.Natural
 import qualified Prelude
 
@@ -23,10 +28,32 @@ import qualified Prelude
     Runtime definitions
 ------------------------------------------------------------------------------}
 
+class ToTyp a where
+    toTyp :: Proxy a -> Typ
+
+instance ToTyp Prelude.Bool where toTyp _ = Typ.Zero Typ.Bool
+instance ToTyp Data.ByteString.ByteString where toTyp _ = Typ.Zero Typ.Bytes
+instance ToTyp Prelude.Integer where toTyp _ = Typ.Zero Typ.Integer
+instance ToTyp Numeric.Natural.Natural where toTyp _ = Typ.Zero Typ.Natural
+instance ToTyp Data.Text.Text where toTyp _ = Typ.Zero Typ.Text
+instance ToTyp () where toTyp _ = Typ.Zero Typ.Unit
+instance forall a. (ToTyp a) => ToTyp (Prelude.Maybe a) where
+    toTyp _ = Typ.One Typ.Option (toTyp (Proxy @a))
+instance forall a. (ToTyp a) => ToTyp [a] where
+    toTyp _ = Typ.One Typ.Sequence (toTyp (Proxy @a))
+instance forall a. (ToTyp a) => ToTyp (Data.Set.Set a) where
+    toTyp _ = Typ.One Typ.PowerSet (toTyp (Proxy @a))
+instance forall a b. (ToTyp a, ToTyp b) => ToTyp (Prelude.Either a b) where
+    toTyp _ = Typ.Two Typ.Sum2 (toTyp (Proxy @a)) (toTyp (Proxy @b))
+instance forall a b. (ToTyp a, ToTyp b) => ToTyp (a, b) where
+    toTyp _ = Typ.Two Typ.Product2 (toTyp (Proxy @a)) (toTyp (Proxy @b))
+instance forall a b. (ToTyp a, ToTyp b) => ToTyp (Data.Map.Map a b) where
+    toTyp _ = Typ.Two Typ.PartialFunction (toTyp (Proxy @a)) (toTyp (Proxy @b))
+
 -- | Class of types that can be converted to 'Value'.
 --
 -- Note: The 'Ord' constraint is necessary to deal with 'Set'.
-class Prelude.Ord a => ToValue a where
+class (Prelude.Ord a, ToTyp a) => ToValue a where
     toValue :: a -> V.Value
 
 z :: V.ZeroF -> V.Value
@@ -39,7 +66,7 @@ instance ToValue Numeric.Natural.Natural where toValue = z . V.Natural
 instance ToValue Data.Text.Text where toValue = z . V.Text
 instance ToValue () where toValue _ = z V.Unit
 
-instance ToValue a => ToValue (Prelude.Maybe a) where
+instance (ToValue a) => ToValue (Prelude.Maybe a) where
     toValue = V.One . V.Option . Prelude.fmap toValue
 
 instance ToValue a => ToValue [a] where
@@ -56,9 +83,10 @@ instance (ToValue a, ToValue b) => ToValue (a, b) where
     toValue (a, b) = V.Product [toValue a, toValue b]
 
 instance (ToValue a, ToValue b) => ToValue (Data.Map.Map a b) where
+    toValue :: Data.Map.Map a b -> V.Value
     toValue =
         V.Two
             . V.FiniteMap
             . Data.Map.fromList
-            . Prelude.map (\(k, v) -> (toValue k, toValue v))
+            . Prelude.map (bimap toValue toValue)
             . Data.Map.toList
