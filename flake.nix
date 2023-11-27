f0de3c8b89e3edfee51c0d368458f2c46be412f4
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2023-11-27 15:17:06 +0100

    Simplify `flake.nix`

--- flake.nix
+++ flake.nix
@@ -6,13 +6,6 @@
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     iohkNix.url = "github:input-output-hk/iohk-nix";
     flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
-
-    CHaP.url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
-    CHaP.flake = false;
-
-    # non-flake nix compatibility
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-compat.flake = false;
   };
 
   outputs = inputs:
@@ -42,15 +35,6 @@
           name = "fine-type";
           compiler-nix-name = "ghc928";
 
-          # CHaP input map, so we can find CHaP packages (needs to be more
-          # recent than the index-state we set!). Can be updated with
-          #
-          #  nix flake lock --update-input CHaP
-          #
-          inputMap = {
-            "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.CHaP;
-          };
-
           # tools we want in our shell
           shell.tools = {
             cabal = "3.10.1.0";
@@ -67,51 +51,12 @@
             gitAndTools.git
           ];
           shell.withHoogle = true;
-
-          # package customizations as needed. Where cabal.project is not
-          # specific enough, or doesn't allow setting these.
-          modules = [
-            ({pkgs, ...}: {
-              # Packages we wish to ignore version bounds of.
-              # This is similar to jailbreakCabal, however it
-              # does not require any messing with cabal files.
-              packages.katip.doExactConfig = true;
-
-              # split data output for ekg to reduce closure size
-              packages.ekg.components.library.enableSeparateDataOutput = true;
-              packages.cardano-binary.configureFlags = [ "--ghc-option=-Werror" ];
-              packages.cardano-crypto-class.configureFlags = [ "--ghc-option=-Werror" ];
-              packages.slotting.configureFlags = [ "--ghc-option=-Werror" ];
-              enableLibraryProfiling = profiling;
-            })
-            ({pkgs, ...}: with pkgs; nixpkgs.lib.mkIf stdenv.hostPlatform.isWindows {
-              packages.text.flags.simdutf = false;
-              # Disable cabal-doctest tests by turning off custom setups
-              packages.comonad.package.buildType = lib.mkForce "Simple";
-              packages.distributive.package.buildType = lib.mkForce "Simple";
-              packages.lens.package.buildType = lib.mkForce "Simple";
-              packages.nonempty-vector.package.buildType = lib.mkForce "Simple";
-              packages.semigroupoids.package.buildType = lib.mkForce "Simple";
-
-              # Make sure we use a buildPackages version of happy
-              # packages.pretty-show.components.library.build-tools = [ (pkgsBuildBuild.haskell-nix.tool compiler-nix-name "happy" "1.20.1.1") ];
-
-              # Remove hsc2hs build-tool dependencies (suitable version will be available as part of the ghc derivation)
-              packages.Win32.components.library.build-tools = lib.mkForce [];
-              packages.terminal-size.components.library.build-tools = lib.mkForce [];
-              packages.network.components.library.build-tools = lib.mkForce [];
-            })
-          ];
         }).flake (
           # we also want cross compilation to windows.
           nixpkgs.lib.optionalAttrs (system == "x86_64-linux") {
           crossPlatforms = p: [p.mingwW64];
         });
-      in nixpkgs.lib.recursiveUpdate flake {
-        # add a required job, that's basically all hydraJobs.
-        # hydraJobs = nixpkgs.callPackages inputs.iohkNix.utils.ciJobsAggregates
-        #  { ciJobs = flake.hydraJobs; };
-      }
+      in flake
     );
 
   nixConfig = {
